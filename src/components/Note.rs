a77f696d1338980b99542ed62da3cc42c8745baf
SamDeCqa <samwelsikanyika88@gmail.com>
2026-07-31 18:47:36 +0300

    modified Notes file

--- src/components/Note.rs
+++ src/components/Note.rs
@@ -1,7 +1,21 @@
-struct Note {
+pub struct Note {
     id : u32,
-    title : str,
-    body : str,
-    created_at : str,
-    updated_at : str
+    title : String,
+    body : String,
+    created_at : String,
+    updated_at : String
+}
+
+impl Note {
+    fn create(){
+        println!("You are creating a Note")
+    }
+
+    fn update(&self) {
+        println!("The Note with the title '{}' is to be edited", self.title)
+    }
+
+    fn delete(&self) {
+        println!("Do you want to DELETE the Note with the title '{}'?", self.title)
+    }
 }
