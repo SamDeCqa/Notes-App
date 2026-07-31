df09db4a331619c40153d414adc257ef9d3c2433
SamDeCqa <samwelsikanyika88@gmail.com>
2026-07-31 16:10:30 +0300

    Menu creation

---
+++ src/components/welcome.rs
@@ -0,0 +1,28 @@
+fn greetings(){
+    println!("Hey there! What's up?");
+    println!("What do you wanna do?")
+}
+
+enum MENU {
+    Browse,
+    Create,
+    Update,
+    Delete
+}
+
+fn menu_descriptions(){
+    match MENU {
+        MENU::Browse => {
+            println!("Looking for any existing notes ....")
+        },
+        MENU::Create => {
+                        println!("Creating a new note ....")
+        },
+        MENU::Update => {
+            println!("Updating a Note, Please wait ....")
+        },
+        MENU::Delete => {
+            println!("Sorry! Coulld not delete note ....")
+        },
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut name = String::from("Samwel");
+    println!("Hello, there!");
+    println!("Hello, {}!", &name);
+    
+    name = String::from("Developer");
+    println!("Your name is, {}!", &name);
+}
+
+fn welcome_menu(str: choice) -> str {
+
+    match  {
+      op  
+    }
+}
