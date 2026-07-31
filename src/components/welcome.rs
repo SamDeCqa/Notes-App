4a60d551ba92ec4add06cb77e1b20329f56de5ed
SamDeCqa <samwelsikanyika88@gmail.com>
2026-07-31 18:30:54 +0300

    importing welcome and menu to main file

--- src/components/welcome.rs
+++ src/components/welcome.rs
@@ -1,3 +1,5 @@
+use std::{io::stdin};
+
 enum MENU {
     Browse,
     Create,
@@ -5,15 +7,43 @@ enum MENU {
     Delete,
 }
 
+pub fn show(){
+    let mut  opt : String = String::new();
+
+    greetings();
+
+    menu_list();
+
+    stdin().read_line( &mut opt).expect("Unable to read input");
+
+    let choosen = match opt.trim() {
+        "1" => MENU::Browse,
+        "2" => MENU::Create,
+        "3" => MENU::Update,
+        "4" => MENU::Delete,
+        _ => {
+            println!("Unkown input. Defaulting to browse");
+            MENU::Browse
+        }
+    };
+
+    menu_descriptions(choosen);
+    
+}
 
 fn greetings() {
     println!("Hey there! What's up?");
-    println!("What do you wanna do?")
+    println!("What do you wanna do?");
+}
+
+fn menu_list() {
+    println!(" 1. Browse \n 2. Create \n 3. Edit \n 4. Delete \n 0. Exit");
 }
 
 
-fn menu_descriptions() {
-    match MENU {
+
+fn menu_descriptions(choice : MENU) {
+    match choice {
         MENU::Browse => {
             println!("Looking for any existing notes ....")
         }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+mod components;
+
+use components::welcome::{
+    show
+};
+
 fn main() {
-    let mut name = String::from("Samwel");
+    /*let mut name = String::from("Samwel");
     println!("Hello, there!");
     println!("Hello, {}!", &name);
     
     name = String::from("Developer");
-    println!("Your name is, {}!", &name);
+    println!("Your name is, {}!", &name);*/
+    show();
 }
 
